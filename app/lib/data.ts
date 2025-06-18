d23baef7ee48fcb088d9edd1238c58bc2002af94
Austin Oligario <austinoligario@gmail.com>
2025-06-18 00:10:55 -0400

    feat: add mock player data retrieval function

---
+++ app/lib/data.ts
@@ -0,0 +1,6 @@
+import { PlayerSeason } from '../types';
+import { mockPlayerSeason } from '../data/mockPlayerData';
+
+export function getMockPlayerData(): PlayerSeason {
+	return mockPlayerSeason;
+}
