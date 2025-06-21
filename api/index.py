106c4265599383f172b17bc70578579c71250d22
Austin Oligario <austinoligario@gmail.com>
2025-06-21 01:53:34 -0400

    more testing

--- api/index.py
+++ api/index.py
@@ -1,21 +1,15 @@
-from flask import Flask
 from nba_api.stats.endpoints import playercareerstats
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','application/json')
+        self.end_headers()
 
-app = Flask(__name__)
+        # Nikola Jokić
+        career = playercareerstats.PlayerCareerStats(player_id='203999') 
+        self.wfile.write(career.get_json().encode('utf-8'))
 
-@app.route('/api/player')
-def home():
-    # Nikola Jokić
-    career = playercareerstats.PlayerCareerStats(player_id='203999') 
-    # # pandas data frames (optional: pip install pandas)
-    # career.get_data_frames()[0]
-    # # json
-    #
-    # # dictionary
-    # career.get_dict()
-
-    return career.get_json()
-
-@app.route('/api/about')
-def about():
-    return 'About'
+        return 
